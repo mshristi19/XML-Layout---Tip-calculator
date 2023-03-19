ba99736a55a2d2ddf994347e68859036ecca5845
mshristi19 <mshristi19@tbc.edu.np>
2023-03-19 14:30:04 +0545

    Completed UI Design

--- app/src/main/java/com/example/converter/MainActivity.kt
+++ app/src/main/java/com/example/converter/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.converter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
